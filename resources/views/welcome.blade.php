47f0341b9c702aec1818cdb72e2718f090afcefb
EC2 Default User <ec2-user@ip-172-31-26-57.us-east-2.compute.internal>
2018-04-20 11:37:35 +0000

    set timezone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TopPage
                 </div>
 
                 <div class="links">
