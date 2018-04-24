a2fe3ab4654ba58a7390707304d41f33dbb135f1
EC2 Default User <ec2-user@ip-172-31-26-57.us-east-2.compute.internal>
2018-04-24 13:29:49 +0000

    add status column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -33,8 +33,12 @@ class TasksController extends Controller
     // postでmessages/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            ]);
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect('/');
@@ -63,7 +67,12 @@ class TasksController extends Controller
     // putまたはpatchでmessages/idにアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            ]);
+            
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,7 +4,8 @@
 
 <!-- ここにページ毎のコンテンツを書く -->
 <h1>id = {{ $task->id }}のタスク詳細ページ</h1>
-<p>{{ $task->content }}</p>
+<p>メッセージ: {{ $task->content }}</p>
+<p>状態: {{ $task->status }}</p>
 {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
 
 {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
